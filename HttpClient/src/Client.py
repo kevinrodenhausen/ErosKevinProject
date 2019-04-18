7ad5f5aeaf0e3b6b048a6500d526989b96785997
kevinrodenhausen <47259568+kevinrodenhausen@users.noreply.github.com>
2019-04-18 18:21:26 -0500

    Add files via upload
    So sometimes this works (Well at the end when it asks for user input it will break) but other times the server will send messages too fast and the client receives two messages in 1 (specifically 1file1 is a possible error). But this still kinda has the flow I was talking about when sending and receiving the contents of the file.

--- HttpClient/src/Client.py
+++ HttpClient/src/Client.py
@@ -9,11 +9,14 @@ from socket import *
 
 def main():
     clientSocket = setConnection()
+    requestDirectory(clientSocket)
     userInput = input('Type 1 if you want to request a directory.')
+    int(userInput)
+    print(userInput)
         
     while(userInput == 1):
-        fileAmount = requestDirectory(clientSocket)
-        downloadContent(clientSocket, fileAmount)
+        requestDirectory(clientSocket)
+        userInput = -1
         userInput = input('Type 1 if you want to request a directory.')
         
     clientSocket.close()
@@ -21,7 +24,7 @@ def main():
     
 #Set up the connection to start communication between client and server
 def setConnection():
-    serverName = 'localHost' #This will have to be changed to the actual server name
+    serverName = 'localhost' #This is specific for my machine you will have to change it!
     serverPort = 60040
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((serverName,serverPort))
@@ -31,42 +34,48 @@ def setConnection():
 def requestDirectory(clientSocket):
     #feedBack returns how many files are in the directory or -1 if there is no directory by that name.
     feedBack = -1 
-    
+    print('In requestDirectory')
     while(feedBack == -1):
         directoryName = input('Please specify a valid directory name\n')
         clientSocket.send(directoryName.encode())
-        feedBack = clientSocket.recv(1024).decode()
-    
-    return feedBack
+        feedBack = int(clientSocket.recv(1024).decode())
+        print('feedBack is: ', feedBack)
+        
+    downloadContent(clientSocket,feedBack,directoryName)
 
 #Copies the content of all the files into various files.
-def downloadContent(clientSocket, fileAmount):
+def downloadContent(clientSocket, fileAmount, directoryName):
     #First make a directory to hold all the files
     #This won't work if intermediate directories don't exist...
     #This path is specific for my computer, and this may not be implemented this way.
     #A function called make mkdirs can be used to make a path
     #Users may not want this program creating random directories on their machine...
-    path = 'C:\\Users\\Kevin\\Documents\\Communication Networks\\Random Files'
-    counter = 1
-    fileNumber = str(counter)
-    fileName = '\\file'
-    fileEnd = -1
-    condition = True
+    #I'm using Temp but when it's actually implemented it will be directoryName
+    path = 'C:\\Users\\Kevin\\Documents\\Communication Networks\\' + 'Temp\\'
+    stringCounter = clientSocket.recv(1024).decode()
+    print('Counter is: ' + stringCounter)
+    counter = int(stringCounter)
+    #fileNumber = str(counter)
+    EOF = 'KevinEros'
     
     #Hopefully this will be in sync with the server when reading files
-    while(counter < fileAmount):
-        fullFilename = fileName + fileNumber
-        fullPath = path + fullFilename
+    while(counter <= fileAmount):
+        fileName = clientSocket.recv(1024).decode()
+        print(fileName)
+        fullPath = path + fileName
+        print(fullPath)
         file = open(fullPath,"a")
         
-        #Infinite loop here
-        #How do we check that server is done with one file and is about to read the contents of the next?
-        while(condition):
+        #Reading a line from the server and writing to the file.
+        while True:
             line = clientSocket.recv(1024).decode()
+            print(line)
+            if(EOF == line):
+                break
+            
+            file.write(line)
         
-        file.write(line)
         counter += 1
-        fileNumber = str(counter)
         
         
     

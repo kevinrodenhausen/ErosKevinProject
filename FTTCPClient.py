588d2d3bef540f83182b5d08abf2945c8f76ef0d
erosMH <48422357+erosMH@users.noreply.github.com>
2019-04-25 18:27:18 -0500

    Add files via upload

---
+++ FTTCPClient.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 25 01:02:48 2019
+
+@author: erosg
+"""
+import socket
+from socket import *
+import sys
+
+if len(sys.argv) != 2:
+    print("Required Format: python FTTCPClient.py <server ip>")
+    sys.exit()
+
+clientSocket = socket(AF_INET, SOCK_STREAM) # Create TCP socket object
+host = sys.argv[1] # IP Address of TCPServer
+port = 50000
+
+clientSocket.connect((host, port))
+if input("Would you like to request a specific file or directory? (F / D): ") == "F":
+    targetFile = input("What is the name of the file? (include '.txt'): ")
+    clientSocket.send((targetFile).encode())
+    with open(targetFile, 'wb') as f:
+        print('file opened')
+        while True:
+            print('receiving data...')
+            data = clientSocket.recv(1024)
+            if str(data.decode()).startswith("Server could not"):
+                f.close()
+                print("File not found on server, file transfer aborted.")
+            print('data=%s', (data))
+            if not data:
+                break
+            f.write(data)
+    f.close()
+    print('Successfully get the file')
+    clientSocket.close()
+    print('connection closed')
+else:
+    targetDirectory = input("What is the name of the directory?: ")
+    clientSocket.send((targetDirectory).encode())
+    filesList = clientSocket.recv(1024)
+    if str(filesList.decode()).startswith("Server could not"):
+        print(str(filesList.decode))
+        clientSocket.close()
+    else:
+        print(str(filesList.decode()).split())
+        clientSocket.close()
+        for file in filesList.decode().split():
+            clientSocket = socket(AF_INET, SOCK_STREAM) # Create TCP socket object        
+            host = sys.argv[1] # IP Address of TCPServer
+            port = 50000
+            clientSocket.connect((host, port))
+            clientSocket.send((targetDirectory + "\\" + file).encode())
+            
+            with open(file, 'wb') as f:
+                print('file opened')
+                while True:
+                    print('receiving data...')
+                    data = clientSocket.recv(1024)
+                    print('data=%s', (data))
+                    if not data:
+                        break
+                    f.write(data)
+            f.close()
+            print('Successfully get the file')
+            clientSocket.close()
+            print('connection closed')
---
+++ FTTCPServer.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 25 01:04:08 2019
+
+@author: erosg
+"""
+
+import socket  
+from socket import *   
+import os
+
+port = 50000
+serverSocket = socket(AF_INET, SOCK_STREAM)
+host = "" # Use localhost
+serverSocket.bind((host, port))            
+serverSocket.listen(5)
+print('Server listening....')
+
+while True:
+    conn, addr = serverSocket.accept()     # Establish connection with client.
+    print('Got connection from', addr)
+    data = conn.recv(1024)
+    if str(data.decode()).endswith(".txt"):  
+        print("Server received", data.decode())
+        try:
+            f = open(data.decode(),'rb')
+            l = f.read(1024)
+            while (l):
+               conn.send(l)
+               print('Sent ',repr(l))
+               l = f.read(1024)
+            f.close()
+    
+            print('Done sending')
+            #conn.send('Thank you for connecting')
+        except:
+            conn.send(("Server could not find file: " + str(data.decode())))
+        conn.close()
+    
+    else:
+        print('Server received', str(data.decode()))
+        directoryName = str(data.decode())
+        path = os.getcwd()
+        directoryPath = path + "\\" + directoryName
+        print(directoryPath)
+        try:
+            directoryList = os.listdir(directoryPath)
+            print(" ".join(directoryList))
+            conn.send((" ".join(directoryList)).encode())
+        except:
+            conn.send(("Server could not find directory: " + directoryName).encode())
+        conn.close()
